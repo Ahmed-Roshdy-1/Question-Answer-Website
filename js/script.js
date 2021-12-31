7ea4cafb26ae3700062514c77111b25d2aa9bf8f
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-31 02:34:20 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// // Notice there is no 'import' statement. 'qna' and 'tf' is
-// // available on the index-page because of the script tag above.
 // const predict = async () => {
 //   const data = document.getElementById("data");
 //   const question = document.getElementById("question");
@@ -21,17 +19,26 @@
 
 // predict();
 
-// Notice there is no 'import' statement. 'qna' and 'tf' is
-// available on the index-page because of the script tag above.
+const data = document.getElementById("data");
+const question = document.getElementById("question");
+const submit = document.getElementById("submit");
+const answer = document.getElementById("answer");
+
 const predict = async () => {
   model = await qna.load();
 
-  document.getElementById("submit").addEventListener("click", (e) => {
-    const passage = document.getElementById("data").innerHTML;
-    const question = document.getElementById("question").value;
-    model.findAnswers(question, passage).then((answers) => {
+  submit.addEventListener("click", (e) => {
+    // const passage = document.getElementById("data").innerHTML;
+    // const question = document.getElementById("question").value;
+
+    model.findAnswers(question.value, data.innerHTML).then((answers) => {
       console.log("Answers: ", answers);
-      document.getElementById("answer").innerHTML = answers[0].text;
+      if (answers[0]) {
+        answer.innerHTML = answers[0].text;
+      } else {
+        answer.innerHTML = "The question is not correct, check the syntax or ask a question about the paragraph";
+        answer.style.color = "#d00000";
+      }
     });
 
     // Prevent submitting the form which would cause a page reload.
